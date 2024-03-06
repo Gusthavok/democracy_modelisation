d8114fb547de025efb9d3f64bacd0c7a89947e23
Augustin Kheng <augustin.sophana.kheng@gmail.com>
2024-03-06 20:02:19 +0100

    unfonctionnal commit

--- individu.py
+++ individu.py
@@ -2,9 +2,14 @@ import numpy as np
 
 class Individu:
     def __init__(self, taille_opinion:int, taille_statut_social:int) -> None:
-        self.opinion
-        self.statut_social
+        self.opinion = np.array([0 for j in range(taille_opinion)])
+        self.statut_social = np.array([0 for j in range(taille_statut_social)])
 
-        self.influence
-        self.sociabilisation
+        self.influence = 1
+        self.sociabilisation = 1
+
+    def set_completement_aleatoire():
+        pass
+    def set_representatif_de_la_realite():
+        pass
     
--- population.py
+++ population.py
@@ -1,16 +1,34 @@
 import individu
 import candidats
 
-
-
 class Population:
 
-    def __init__(self, opinion:Opinion) -> None:
-        self.individus # liste d'individus
-        self.candidats # liste des candidats
+    def __init__(self) -> None:
+        self.individus = [] # liste d'individus
+        self.candidats = [] # liste des candidats
 
-        # D'autres parametre liés au type de société qu'on représente : beaucoup ou pas beaucoup d'intéraction, société très polarisée ou pas trop...
-        #self. ...
+        # parametre liés au type de société qu'on représente : beaucoup ou pas beaucoup d'intéraction, société très polarisée ou pas trop...
+        
+        self.ecart_type_influence = 1
+        self.ecart_type_sociabilisation = 1
+
+    def initialisation_aleatoire_population(self, taille_population:int, taille_opinion:int, taille_statut_social:int, type_population:int = "completement_aleatoire"):
+        if len(self.individus) != 0:
+            raise ValueError("Initialisation aléatoire sur une population non vide")
+        
+        if type_population == "completement_aleatoire":
+            self.individus = [individu.Individu(taille_opinion, taille_statut_social).set_completement_aleatoire() for _ in range(taille_population)]
+        elif type_population == "representatif_de_la_realite":
+            self.individus = [individu.Individu(taille_opinion, taille_statut_social).set_representatif_de_la_realite() for _ in range(taille_population)]
+        else:
+            raise ValueError("le type de population utilisé n'est pas valide")
+
+    ## différents modèles représentants l'arrivée des candidats en politique
+    def placement_candidats_type_1():
+        pass
+    
+    def placement_candidats_type_1():
+        pass
 
     def etape_temporelle(self):
         pass
@@ -18,11 +36,16 @@ class Population:
     
     def affiche(self):
         pass
-
+    
+    ## différents types d'élections possible
     def election_type_1(self):
+        if len(self.candidats) == 0:
+            raise ValueError("Impossible de réaliser une élection avec aucun candidats")
         pass
 
     def election_type_2(self):
+        if len(self.candidats) == 0:
+            raise ValueError("Impossible de réaliser une élection avec aucun candidats")
         pass
 
 def interaction(self): # A refaire
