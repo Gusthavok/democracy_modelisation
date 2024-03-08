8a1bed6264ec0aebeb877033d98347084edeeb30
Matthieu <matthieu.bruant@gmail.com>
2024-03-08 21:25:02 +0100

    a

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import population
 
 pops = population.Population()
 
-pops.initialisation_aleatoire_population(1000, 2, 0)
-pops.affiche_1(nom_fichier = "fichier_de_base")
+pops.initialisation_aleatoire_population(1000, 2, 1)
+pops.affiche_1(nom_fichier = "imgs/fichier_de_base")
 
 
-for ind in range(30):
+for ind in range(60):
     pops.evolution(5)
-    pops.affiche_1(nom_fichier = "fichier_"+str(ind))
+    pops.affiche_1(nom_fichier = "imgs/fichier_"+str(ind))
 
 pops.initialisation_aleatoire_candidats(6)
 pops.placement_strategique_candidats(1, population.Population.election_type_1, nombre_iteration=20)
@@ -17,3 +17,6 @@ couleurs_votants = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow']
 couleurs_candidats = ['black' for _ in range(6)]
 
 pops.affiche_2(couleurs_votants, couleurs_candidats)
+pops.plot_opinion_statut()
+
+print('ratio pos/neg : ', population.nb_pos/population.nb_neg)
--- population.py
+++ population.py
@@ -5,10 +5,12 @@ import candidat
 
 coef_pos = 1.05 #à quel point une interaction positive te fais changer d'avis
 coef_neg = 1.1 #à quel point une interaction négative te fais changer d'avis
-c_opinion = 1 #à quel point des différences d'opinion produisent des interactions négatives
-c_place = 1 #à quel point des différences de place dans la société produisent des interactions négatives
+c_opinion = 0.1 #à quel point des différences d'opinion produisent des interactions négatives
+c_place = 2 #à quel point des différences de place dans la société produisent des interactions négatives
 c_place_choix_interactions = 1 #à quel point être distant socialement t'empèche d'interagir
 
+nb_neg = 0
+nb_pos = 0
 
 class Population:
 
@@ -24,6 +26,9 @@ class Population:
         self.taille_place_societe = 0
         
         self.abstention_factor = 0.2
+
+        self.nb_neg = 0
+        self.nb_pos = 0
 
     def initialisation_aleatoire_population(self, taille_population:int, taille_opinion:int, taille_place_societe:int, type_population:str = "completement_aleatoire"):
 
@@ -79,20 +84,30 @@ class Population:
                             s.programme_publique[j] += pas_moins
     
     def etape_temporelle(self):
-        for a in self.individus:
-            for _ in range(a.sociabilisation):
-                p = [np.exp(-c_place_choix_interactions*np.linalg.norm(a.place_societe - i.place_societe)) for i in self.individus]
-                interaction(a,np.random.choice(self.individus, p = p/np.sum(p)))
+        for a in range(len(self.individus)):
+            for _ in range(self.individus[a].sociabilisation):
+                b = np.random.choice(len(self.individus))
+                if np.random.binomial(1, self.proba_interactions[a][b]):
+                    interaction(self.individus[a],self.individus[b])
         
     def evolution(self,n):
+        if not hasattr(self, 'proba_interactions'):
+            p = [[np.exp(-c_place_choix_interactions*np.linalg.norm(a.place_societe - b.place_societe)) for a in self.individus] for b in self.individus]
+            self.proba_interactions = p
         for _ in range(n):
             self.etape_temporelle()
     
     def affiche_1(self, nom_fichier = ""):
-        if self.taille_opinion > 2:
-            raise ValueError("impossible de plot en dimension > 2")
-        plt.scatter([a.opinion[0] for a in self.individus],[a.opinion[1] for a in self.individus], c = "blue")
-        plt.scatter([s.programme_publique[0] for s in self.candidats],[s.programme_publique[1] for s in self.candidats], c = "red")
+        if self.taille_opinion > 3:
+            raise ValueError("impossible de plot en dimension > 3")
+        if self.taille_opinion == 3:
+            ax = plt.axes(projection='3d')
+        if self.taille_opinion == 2:  
+            ax = plt.axes()
+        pts = [[a.opinion[i] for a in self.individus] for i in range(self.taille_opinion)]
+        ax.scatter(*pts, c = "blue")
+        pts = [[a.programme_public[i] for a in self.candidats] for i in range(self.taille_opinion)]
+        ax.scatter(*pts, c = "red")
         if nom_fichier != "":
             plt.savefig(nom_fichier+'.png')
         else:
@@ -101,9 +116,13 @@ class Population:
 
     
     def affiche_2(self, couleurs_individus, couleurs_candidats):
-        if self.taille_opinion > 2:
-            raise ValueError("impossible de plot en dimension > 2")
-        
+        if self.taille_opinion > 3:
+            raise ValueError("impossible de plot en dimension > 3")
+        if self.taille_opinion == 3:
+            ax = plt.axes(projection='3d')
+        if self.taille_opinion == 2:  
+            ax = plt.axes()
+
         def eq(indiv, candi):
             som = 0
             for indice in range(self.taille_opinion):
@@ -124,10 +143,14 @@ class Population:
             else:
                 abstentionistes.append(indiv)
         
-        plt.scatter([a.opinion[0] for a in abstentionistes],[a.opinion[1] for a in abstentionistes], c = 'gray')
+        
+        pts = [[a.opinion[i] for a in abstentionistes] for i in range(self.taille_opinion)]
+        ax.scatter(*pts, c = "gray")
         for ind in range(len(self.candidats)):
-            plt.scatter([a.opinion[0] for a in votants[ind]],[a.opinion[1] for a in votants[ind]], c = couleurs_individus[ind])
-            plt.scatter([self.candidats[ind].programme_publique[0]],[self.candidats[ind].programme_publique[1]], c = couleurs_candidats[ind])
+            pts = [[a.opinion[i] for a in votants[ind]] for i in range(self.taille_opinion)]
+            ax.scatter(*pts, c = couleurs_individus[ind])
+            pts = [[self.candidats[i].programme_publique[0]] for i in range(self.taille_opinion)]
+            ax.scatter(*pts, c = couleurs_candidats[ind])
         
         plt.show()
         plt.cla()
@@ -157,14 +180,26 @@ class Population:
             raise ValueError("Impossible de réaliser une élection sans candidats")
         pass
 
+    def plot_opinion_statut(self):
+        ax = plt.axes(projection = '3d')
+        ax.scatter([a.opinion[0] for a in self.individus], [a.opinion[1] for a in self.individus], [a.place_societe[0] for a in self.individus])
+        plt.show()
+
 def interaction(a,b):
+    global nb_neg, nb_pos
     u = np.random.binomial(1, np.exp( -c_opinion*np.linalg.norm(a.opinion - b.opinion) - c_place*np.linalg.norm(a.place_societe - b.place_societe)))
     m = (a.influence*a.opinion + b.influence*b.opinion)/(a.influence + b.influence)
     if u:
+        nb_pos += 1
         a.opinion += coef_pos*(m - a.opinion) # se rapproche du barycentre
         b.opinion += coef_pos*(m - b.opinion)
     else:
-        a.opinion = np.array([min(1.0, max(0.0, a.opinion[ind] - coef_neg*(m[ind] - a.opinion[ind]))) for ind in range(len(a.opinion))]) # s'écarte par rapport au barycentre
-        b.opinion = np.array([min(1.0, max(0.0, b.opinion[ind] - coef_neg*(m[ind] - b.opinion[ind]))) for ind in range(len(b.opinion))])
+        #a.opinion = np.array([min(1.0, max(0.0, a.opinion[ind] - coef_neg*(m[ind] - a.opinion[ind]))) for ind in range(len(a.opinion))]) # s'écarte par rapport au barycentre
+        #b.opinion = np.array([min(1.0, max(0.0, b.opinion[ind] - coef_neg*(m[ind] - b.opinion[ind]))) for ind in range(len(b.opinion))])
+        a.opinion -= coef_neg*(m - a.opinion) # s'eloigne du barycentre
+        b.opinion -= coef_neg*(m - b.opinion)
+        a.opinion = np.clip(a.opinion, 0, 1)
+        b.opinion = np.clip(b.opinion, 0, 1)
+        nb_neg += 1
         
     
